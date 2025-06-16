dfe16a798b6743746ebe613e51e1ef5f1913bca4
Acox <snsdkaskusone@gmail.com>
2025-06-16 22:58:03 +0700

    feat: add responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,10 @@ import Input from "@/components/Input";
 import { useState, useRef, useEffect } from "react";
 
 let qrCode: {
-  download(arg0: { extension: string; }): unknown;
+  download(arg0: { name: string; extension: string; }): unknown;
   update(arg0: { data: string; }): unknown; append: (arg0: HTMLDivElement) => void;
 };
+
 if (typeof window !== "undefined") {
   const QRCodeStyling = require("qr-code-styling");
   qrCode = new QRCodeStyling({
@@ -51,7 +52,7 @@ if (typeof window !== "undefined") {
 }
 
 export default function Home() {
-  const [first, setFirst] = useState(true)
+  const [qr, setQr] = useState("")
   const [name, setName] = useState("")
   const [size, setSize] = useState("")
 
@@ -68,12 +69,16 @@ export default function Home() {
       data: `${name} [Size : ${size}]`,
     });
 
-    setFirst(false)
+    setQr(`${name} [Size : ${size}]`)
   }
 
   const handleDownload = () => {
+    qrCode.update({
+      data: qr,
+    });
     qrCode.download({
-      extension: "png",
+      name: qr.replace("Size : ", ""),
+      extension: "jpg",
     });
   }
 
@@ -81,7 +86,7 @@ export default function Home() {
     <div className="w-full h-dvh flex justify-center py-24 bg-gray-100">
       <div className="p-8 bg-white shadow rounded-2xl h-fit">
         <h1 className="text-center font-bold mb-8 text-xl">HA TIRE | QR Code Generator</h1>
-        <div className="flex gap-8">
+        <div className="flex gap-8 flex-wrap justify-center">
           <div className="w-[300px] flex flex-col justify-center">
             <label className="font-semibold text-sm mb-1 mt-4 block">Product Name</label>
             <Input value={name} onChange={(value) => setName(value)} placeholder="Input product name" />
@@ -90,7 +95,12 @@ export default function Home() {
             <Input value={size} onChange={(value) => setSize(value)} placeholder="Input product size" />
             <Button onClick={handleButton} className="mt-5">Generate</Button>
           </div>
-          <div ref={qrRef} className={`w-[300px] h-[300px] qrcode ${first ? "bg-red-50" : "bg-white"}`} />
+          <div className="relative group">
+            <div ref={qrRef} className={`w-[300px] h-[300px] qrcode ${qr ? "bg-white" : "bg-red-50"}`} />
+            {qr && <div className="invisible group-hover:visible absolute inset-0 bg-black/50 flex items-center justify-center">
+              <button className="text-white font-bold" onClick={handleDownload}>Download</button>
+            </div>}
+          </div>
         </div>
       </div>
     </div>
